96a63e021c39428b447ed66aed57c2187e2809e5
hailor <hailor1985@gmail.com>
2010-10-28 22:17:15 +0800

    初始化插件

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 # Install hook code here
 %w(charts).each do |dir|
   source = File.expand_path(File.join(File.dirname(__FILE__), 'public', dir))
-  target = File.join(Rails.root, 'public', dir)
+  target = File.join(Rails.root, 'public')
   FileUtils.mkdir_p target
   FileUtils.cp_r(source, target, :verbose => true)
 end
